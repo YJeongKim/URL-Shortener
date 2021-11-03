f0906f4cbed917a2c85e6194e658166dc8483e25
YJeongKim <yjeongisme@gmail.com>
2021-11-03 13:21:03 +0900

    feat: 메인페이지를 위한 home 컨트롤러 추가

---
+++ src/test/java/dev/yjeong/urlshortener/controller/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package dev.yjeong.urlshortener.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("메인페이지 로딩 테스트")
+    void homeTest() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("URL Shortener")));
+    }
+}
