0d0f3ee7052e6139a20bfa3e963a40d7bd97adb2
YJeongKim <yjeongisme@gmail.com>
2021-11-05 01:28:09 +0900

    feat: 메인 페이지 UI 구현 및 API 연결

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,34 @@
+const home = {
+    init : function () {
+        let _this = this;
+        $('#result-div').hide()
+        $('#button-shorten').on('click', function () {
+            _this.shorten();
+        });
+        $('#button-copy').on('click', function () {
+            _this.copy();
+        });
+    },
+    shorten : function () {
+        let data = {
+            url: $("#url").val()
+        }
+        $.ajax({
+            type: "post",
+            url: "/api/urls",
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+        }).done(function (data) {
+            $('#result-div').show()
+            let shortenedUrl = data["shortenedUrl"]
+            $('#result').val(shortenedUrl)
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    copy : function () {
+        navigator.clipboard.writeText($('#result').val()).then(r => alert("copied!"));
+    }
+}
+
+home.init()
